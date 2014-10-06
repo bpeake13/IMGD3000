9d504739ef4b4142e083f7ad463c54d88510b9bc
esfaust <esfaust@gmail.com>
2014-10-06 00:43:16 -0400

    Creature class stub

--- IMGD3000Proj3/src/Creature.cpp
+++ IMGD3000Proj3/src/Creature.cpp
@@ -16,3 +16,50 @@ Creature::~Creature() {
 	// TODO Auto-generated destructor stub
 }
 
+/**
+ * Getter for attack
+ * @return
+ */
+int Creature::getAttack() const {
+	return attack;
+}
+
+/**
+ * Setter for attack
+ * @param attack
+ */
+void Creature::setAttack(int attack) {
+	this->attack = attack;
+}
+
+/**
+ * Getter for health
+ * @return
+ */
+int Creature::getHealth() const {
+	return health;
+}
+
+/**
+ * Setter for health
+ * @param health
+ */
+void Creature::setHealth(int health) {
+	this->health = health;
+}
+
+/**
+ * Getter for sprite
+ * @return
+ */
+const SpriteObject& Creature::getSpr() const {
+	return spr;
+}
+
+/**
+ * Setter for sprite
+ * @param spr
+ */
+void Creature::setSpr(const SpriteObject& spr) {
+	this->spr = spr;
+}
--- IMGD3000Proj3/src/Creature.h
+++ IMGD3000Proj3/src/Creature.h
@@ -9,18 +9,23 @@
 #define CREATURE_H_
 
 #include "Object.h"
+#include "SpriteObject.h"
 
 class Creature: public Object {
 private:
 	int health; //How much damage the creature needs to take to be destroyed
-	int attack; //The creature's physical attack
-	Position pos; //The position on screen
-	Sprite spr; //The sprite of the monster
-
+	int attack; //The creature's attack value
+	SpriteObject spr;
 
 public:
 	Creature();
 	virtual ~Creature();
+	int getAttack() const;
+	void setAttack(int attack);
+	int getHealth() const;
+	void setHealth(int health);
+	const SpriteObject& getSpr() const;
+	void setSpr(const SpriteObject& spr);
 };
 
 #endif /* CREATURE_H_ */
