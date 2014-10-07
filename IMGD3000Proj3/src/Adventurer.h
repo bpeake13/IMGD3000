9ff98486c95a71b47ef41da5edba8dd6f50789d0
esfaust <esfaust@gmail.com>
2014-10-06 23:44:07 -0400

    Stubs for Adventurer, Monster and Creature

---
+++ IMGD3000Proj3/src/Adventurer.h
@@ -0,0 +1,27 @@
+/*
+ * Adventurer.h
+ *
+ *  Created on: Oct 6, 2014
+ *      Author: Eric
+ */
+
+#ifndef ADVENTURER_H_
+#define ADVENTURER_H_
+
+#include "Creature.h"
+
+class Adventurer: public Creature {
+private:
+	void death();
+	int magic; //Magical attack power
+
+public:
+	Adventurer();
+	virtual ~Adventurer();
+	void magicAttack();
+	void physicalAttack();
+	int getMagic() const;
+	void setMagic(int magic);
+};
+
+#endif /* ADVENTURER_H_ */
---
+++ IMGD3000Proj3/src/Monster.h
@@ -0,0 +1,25 @@
+/*
+ * Monster.h
+ *
+ *  Created on: Oct 6, 2014
+ *      Author: Eric
+ */
+
+#ifndef MONSTER_H_
+#define MONSTER_H_
+
+#include "Creature.h"
+
+class Monster: public Creature {
+private:
+	int reward;
+
+public:
+	Monster();
+	virtual ~Monster();
+	void attackPlayer();
+	int getReward() const;
+	void setReward(int reward);
+};
+
+#endif /* MONSTER_H_ */
